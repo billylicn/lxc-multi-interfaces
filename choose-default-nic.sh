dbc1cdcb650307e40191ad7ffbb23870d4d2b1af
billylicn <44922770+billylicn@users.noreply.github.com>
2026-02-13 11:58:05 +0800

    Update choose-default-nic.sh

--- choose-default-nic.sh
+++ choose-default-nic.sh
@@ -4,13 +4,13 @@
 # 颜色定义（兼容不支持颜色的终端）
 # ==============================
 if [[ -t 1 ]]; then
-    RED='\033[0;31m'
-    GREEN='\033[0;32m'
-    YELLOW='\033[1;33m'
-    BLUE='\033[0;34m'
-    CYAN='\033[0;36m'
-    BOLD='\033[1m'
-    NC='\033[0m' # No Color
+    RED=$'\033[0;31m'
+    GREEN=$'\033[0;32m'
+    YELLOW=$'\033[1;33m'
+    BLUE=$'\033[0;34m'
+    CYAN=$'\033[0;36m'
+    BOLD=$'\033[1m'
+    NC=$'\033[0m'
 else
     RED='' GREEN='' YELLOW='' BLUE='' CYAN='' BOLD='' NC=''
 fi
